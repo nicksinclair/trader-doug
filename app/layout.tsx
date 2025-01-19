5f0891d3a037b8b492e086199c845137ff5226b7
Nick Sinclair <nick@hypar.io>
2025-01-19 18:25:31 -0500

    Refactor page, set up query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from 'next'
 import './globals.css'
 
 import localFont from 'next/font/local'
+
+import type { Metadata } from 'next'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -29,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        { children }
       </body>
     </html>
   )
